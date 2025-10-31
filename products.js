59ca1edc69ba4aef238d7166edf10d04b83b9925
Akash Gautam <panditaakashji111@gmail.com>
2025-10-31 22:49:34 +0530

    add items

---
+++ products.js
@@ -0,0 +1,38 @@
+const items = [
+    {
+        id: '001',
+        image: 'images/Modern Sofa.png',
+        item_name: 'Nordic Minimal Sofa',
+        original_price: 1199,
+        current_price: 899,
+        discount_percentage: 25,
+        is_new: true
+    },
+    {
+        id: '002',
+        image: 'images/Coffee Table.png',
+        item_name: 'Rustic Oak Coffee Table',
+        original_price: 399,
+        current_price: 299,
+        discount_percentage: 25,
+        is_new: true
+    },
+    {
+        id: '003',
+        image: 'images/Armchair.png',
+        item_name: 'Vintage Leather Armchair',
+        original_price: 649,
+        current_price: 649,
+        discount_percentage: 0,
+        is_new: true
+    },
+    {
+        id: '004',
+        image: 'images/Floor Lamp.png',
+        item_name: 'Arc Floor Lamp',
+        original_price: 179,
+        current_price: 179,
+        discount_percentage: 0,
+        is_new: true
+    }
+];
