b454cb60a20a0a4313ba805d7a342db1316fd62c
Akash Gautam <panditaakashji111@gmail.com>
2025-11-02 00:23:14 +0530

    added addToCart and updateCartCountBadge functions

--- cart.js
+++ cart.js
@@ -1,22 +1,41 @@
 console.log("inside cart.js");
 const CART_STORAGE_KEY = "mincart_cart";
 
-function getCart(){
+function getCart() {
     let data = localStorage.getItem(CART_STORAGE_KEY);
     return data ? JSON.parse(data) : [];
 }
 
-function saveCart(cart){
-localStorage.setItem(CART_STORAGE_KEY,JSON.stringify(cart));
-updateCartCountBadge();
+function saveCart(cart) {
+    localStorage.setItem(CART_STORAGE_KEY, JSON.stringify(cart));
+    updateCartCountBadge();
 }
 
+function addToCart(product) {
+    let cart = getCart();
+    let existing = cart.find(p => p.id === product.id);
+    if (existing) {
+        existing.qty += 1;
+    }
+    else {
+        cart.push({ ...product, qty: 1 })
+    }
+    saveCart(cart);
+}
 
-function initAddToCartButtons(){
-    document.querySelectorAll(".add-to-cart").forEach((btn)=>{
-        btn.addEventListener("click",()=>{
-            productId = btn.getAttribute("data-id");
-            product = window.items.find(p=>p.id === productId);
+function updateCartCountBadge() {
+    const cart = getCart();
+    const totalItems = cart.reduce((sum, item) => sum + item.qty, 0);
+    const cartcount = document.querySelector(".cart-count");
+    cartcount.textContent = totalItems > 0 ? totalItems : "";
+    cartcount.style.border = totalItems > 0 ? "1px solid #477ea2" : "none";
+}
+
+function initAddToCartButtons() {
+    document.querySelectorAll(".add-to-cart").forEach((btn) => {
+        btn.addEventListener("click", () => {
+            const productId = btn.getAttribute("data-id");
+            const product = window.items.find(p => p.id === productId);
             const cartItem = {
                 id: product.id,
                 name: product.item_name,
