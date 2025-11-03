205ee5eefd29b26269dda3a14b6ca3d726897f58
Akash Gautam <panditaakashji111@gmail.com>
2025-11-03 23:22:03 +0530

    remove product-badge class from badge 2

--- script.js
+++ script.js
@@ -11,17 +11,17 @@ function onloadproducts() {
                 <div class="product-img">
                     <img src="${item.image}" alt="${item.item_name}">
                     ${item.is_new ? '<div class="product-badge badge1">New</div>' : ''}
-                    ${item.is_discount ? `<div class="product-badge badge2">-${Math.round(((item.original_price - item.current_price) / item.original_price) * 100)}%</div>` : ''}
                     <div class="product-actions">
-                        <button class="action-btn" aria-label="Add to Wishlist"></button>
+                    <button class="action-btn" aria-label="Add to Wishlist"></button>
                     </div>
                 </div>
                 <div class="product-info">
                     <h3 class="product-title">${item.item_name}</h3>
                     <div class="product-price">
                         <span class="current-price">$${item.current_price}</span>
-                        ${item.is_discount ? `<span class="old-price">$${item.original_price}</span>` : ''}
-                    </div>
+                        ${item.is_discount ? `<span class="old-price" style="color: #878787;text-decoration: line-through;">$${item.original_price}</span>` : ''}
+                        ${item.is_discount ? `<span class="badge2">${Math.round(((item.original_price - item.current_price) / item.original_price) * 100)}%</span>` : ''}
+                        </div>
                     <button class="add-to-cart" data-id="${item.id}">Add to Cart</button>
                 </div>
             </div>
