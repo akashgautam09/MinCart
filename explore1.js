8e7a73177d8249440ad73947c74cdf51a9c1b73f
Akash Gautam <panditaakashji111@gmail.com>
2025-11-08 00:40:52 +0530

    change the dollar sign to rupees

--- explore1.js
+++ explore1.js
@@ -1,7 +1,7 @@
 function onloadproducts() {
 
     const productsContainer = document.querySelector('.product-grid');
-    if (!productsContainer || !window.items) return;
+    if (!productsContainer || !window.explore1) return;
 
     productsContainer.innerHTML = window.explore1.map(item => `
             <div class="product-card" data-id="${item.id}">
@@ -15,8 +15,8 @@ function onloadproducts() {
                 <div class="product-info">
                     <h3 class="product-title">${item.item_name}</h3>
                     <div class="product-price">
-                        <span class="current-price">$${item.current_price}</span>
-                        ${item.is_discount ? `<span class="old-price" style="color: #878787;text-decoration: line-through;">$${item.original_price}</span>` : ''}
+                        <span class="current-price">₹${item.current_price}</span>
+                        ${item.is_discount ? `<span class="old-price" style="color: #878787;text-decoration: line-through;">₹${item.original_price}</span>` : ''}
                         ${item.is_discount ? `<span class="badge2">${Math.round(((item.original_price - item.current_price) / item.original_price) * 100)}% off</span>` : ''}
                         </div>
                     <button class="add-to-cart" data-id="${item.id}">Add to Cart</button>
